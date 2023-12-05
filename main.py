7649519559b39954f799b4d5e979b1999e43984e
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2023-12-04 19:56:12 -0800

    added script to generate data urls

---
+++ main.py
@@ -0,0 +1,29 @@
+import json
+import io
+import base64
+from glob import glob
+from PIL import Image
+
+
+OUTPUT_PATH = 'blur_data_urls.json'
+BLUR_SIZE = 8
+
+
+urls = {}
+for ext in ('png', 'jpg'):
+    for path in glob(f'images/**/*.{ext}', recursive=True):
+        path = path.replace('\\', '/')
+
+        # Resize image to decrease response size
+        img = Image.open(path).resize((BLUR_SIZE, BLUR_SIZE))
+        output = io.BytesIO()
+        img.save(output, format=ext)
+
+        # Encode resized image binary data into base64
+        data = output.getvalue()
+        b64_string = base64.b64encode(data).decode('utf-8')
+        data_url = f'data:image/jpg;base64,{b64_string}'
+        urls[path] = data_url
+
+with open(OUTPUT_PATH, 'w') as file:
+    json.dump(urls, file, indent=2)
